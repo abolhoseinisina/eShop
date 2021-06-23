49a6da13770bb4d28e81e8f5d8a4c6f1e19a505f
Ebola <abolhoseini.sina@gmail.com>
2021-06-23 23:39:55 +0430

    fix minor naming convention

--- src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -8,16 +8,16 @@ namespace Basket.API.Repositories
 {
     public class BasketRepository : IBasketRepository
     {
-        private readonly IDistributedCache _redisCache;
+        private readonly IDistributedCache redisCache;
 
         public BasketRepository(IDistributedCache redisCache)
         {
-            _redisCache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
+            this.redisCache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
         }
 
         public async Task<ShoppingCart> GetBasket(string userName)
         {
-            var basket = await _redisCache.GetStringAsync(userName);
+            var basket = await redisCache.GetStringAsync(userName);
 
             if (String.IsNullOrEmpty(basket))
                 return null;
@@ -27,14 +27,14 @@ namespace Basket.API.Repositories
 
         public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
         {
-            await _redisCache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+            await redisCache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
 
             return await GetBasket(basket.UserName);
         }
 
         public async Task DeleteBasket(string userName)
         {
-            await _redisCache.RemoveAsync(userName);
+            await redisCache.RemoveAsync(userName);
         }
     }
 }
