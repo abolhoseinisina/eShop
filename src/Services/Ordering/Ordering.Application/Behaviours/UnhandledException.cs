0606e13e2554227670ce5a58a62454c3a0112653
Sina Abolhoseini <abolhoseini.sina@gmail.com>
2021-06-26 14:43:58 +0430

    Creating Ordering.Application project

---
+++ src/Services/Ordering/Ordering.Application/Behaviours/UnhandledException.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Behaviours
+{
+    public class UnhandledException<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger logger;
+
+        public UnhandledException(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                var requestName = typeof(TRequest).Name;
+                logger.LogError(ex, "Application Request: Unhandled Exception for Request {Name} {@Request}", requestName, request);
+                throw;
+            }
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Exceptions;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
+    {
+        private readonly IOrderRepository orderRepository;
+        private readonly IMapper mapper;
+        private readonly ILogger<DeleteOrderCommandHandler> logger;
+
+        public DeleteOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, ILogger<DeleteOrderCommandHandler> logger)
+        {
+            this.orderRepository = orderRepository;
+            this.mapper = mapper;
+            this.logger = logger;
+        }
+
+        public async Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToDelete = await orderRepository.GetByIdAsync(request.Id);
+            if (orderToDelete == null)
+            {
+                logger.LogError("Order not exists in database.");
+                throw new NotFoundException(nameof(Order), request.Id);
+            }
+
+            await orderRepository.DeleteAsync(orderToDelete);
+            logger.LogError("Order is deleted.");
+
+            return Unit.Value;
+        }
+    }
+}
